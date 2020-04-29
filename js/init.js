e4956b5bf2776ba845ff039d6328e8945cd548f4
antoine@antoinealexandre.eu <ComLigue1924>
2020-04-29 11:51:01 +0200

    Modified : - Bug fix : dashboard not showing after logon

--- js/init.js
+++ js/init.js
@@ -1,24 +1,34 @@
-let BACKEND_URL = "http://gestform.ei-bs.eu/";
+let BACKEND_URL = "http://gestform/";
 let isConnected = false;
 let currentUser;
 
 
-$(document).ready(function () {
+$(document).ready(function() {
     $("#loader").fadeOut;
     if (localStorage.getItem('MonToken')) {
         token = localStorage.getItem('MonToken');
         $.ajax({
-            url: BACKEND_URL + "user/getCurrentUser", //Request
-            type: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            success: function (response) {
-                currentUser = response;
-                dashboardChooser(currentUser.roles[0]);
-                $("#userInfo").append("Bonjour " + currentUser.firstname);
-            },
-        });
+                url: BACKEND_URL + "user/getCurrentUser", //Request
+                type: 'GET',
+                headers: {Authorization: `Bearer ${token}`},
+                success: function (response) {
+                    currentUser = response;
+                    dashboardChooser(currentUser.roles[0]);
+                    $("#userInfo").append("Bonjour "+currentUser.firstname);
+                },
+                error: function() {
+                    $.ajax({
+                        url: "login.html",
+                        type: 'GET',
+                        datatype: "HTML",
+                        success: function (response) {
+                            $("#loginInterface").append(response);
+                            login();
+                        }
+                    });
+                }
+            }
+        );
     } else {
         $.ajax({
             url: "login.html",
@@ -53,6 +63,7 @@ function adminDashboard() {
         datatype: "HTML",
         success: function (response) {
             $("#dashboard").append(response);
+            location.hash = 'adminDashboard';
         }
     });
 }
@@ -86,7 +97,7 @@ function showMenu() {
         datatype: "HTML",
         success: function (response) {
             $("#menu").append(response);
-            $("#helloUser").append("Bonjour " + currentUser.firstname);
+            $("#helloUser").append("Bonjour "+currentUser.firstname);
         }
     });
 }
